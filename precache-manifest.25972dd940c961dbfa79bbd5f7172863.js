07cf7bf2692b11b984ba6d58626e19aeb19a47d7
Esha98 <eshashah3335@gmail.com>
2022-03-23 10:36:12 +1000

    Updates

---
+++ precache-manifest.25972dd940c961dbfa79bbd5f7172863.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e922057b799ee34c331357499440922b",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "23c383661450e55dbc8b",
+    "url": "/covid-19/static/css/main.942f695b.chunk.css"
+  },
+  {
+    "revision": "27e3aa28999990e4735f",
+    "url": "/covid-19/static/js/2.ca7fd956.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19/static/js/2.ca7fd956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c383661450e55dbc8b",
+    "url": "/covid-19/static/js/main.6dc66df5.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
